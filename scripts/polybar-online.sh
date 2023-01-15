d1deb03b40f2a64a7afd686b351b03a34adbd2e2
Zero <shinyzero0@gmail.com>
2023-01-15 15:30:52 +0300

    network checker sends notifications

--- scripts/polybar-online.sh
+++ scripts/polybar-online.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 echo "%{F#ff8080}ﴹ"
+connection=false
 while [ true ]
 do
-    wget -T 1 -q --spider https://google.com
+    wget -t 1 -T 1 -q --spider 1.1.1.1
 
     if [ $? -eq 0 ]; then
-        echo "度"
+        if [ "$connection" = false ]; then
+            echo "度"
+            connection=true
+            notify-send -t 5000 "Подключено"
+        fi
     else
-        echo "%{F#ff8080}ﴹ"
+        if [ "$connection" = true ]; then
+            echo "%{F#ff8080}ﴹ"
+            connection=false
+            notify-send -t 5000 -u critical "Соединение потеряно"
+        fi
     fi
     sleep 1
 done
