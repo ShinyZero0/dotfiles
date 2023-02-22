94e870dd2130a268374b6865ab022a6a0425aacf
Zero <shinyzero0@gmail.com>
2023-02-22 15:53:17 +0300

    add hydra, add lazy snip, add lazy border

--- config/nvim/lua/config/lazy-desktop.lua
+++ config/nvim/lua/config/lazy-desktop.lua
@@ -10,43 +10,44 @@ if not vim.loop.fs_stat(lazypath) then
 	})
 end
 vim.opt.runtimepath:prepend(lazypath)
-require("lazy").setup({
 
-	{
-		"nvim-pack/nvim-spectre",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-	},
+require("lazy").setup({  
 	"nvim-lua/plenary.nvim",
+
+  "anuvyklack/hydra.nvim",
+  "anuvyklack/vim-smartword",
 
 	-- VISUAL
 
 	"lukas-reineke/indent-blankline.nvim",
-	"tversteeg/registers.nvim",
+  "folke/which-key.nvim",
 	{
 		"itchyny/lightline.vim",
 		dependencies = {
-			-- {
-			-- 	"challenger-deep-theme/vim",
-			-- 	pin = true,
-			-- },
 		},
 	},
+
 	-- FILETYPES
 
-	"lervag/vimtex",
 	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 	-- "Fymyte/rasi.vim",
+	-- "lervag/vimtex",
 
-	-- FORMATTING
+	-- EDITING
 
 	"windwp/nvim-autopairs",
 	"tpope/vim-commentary",
 	"tpope/vim-surround",
 	"junegunn/vim-easy-align",
-    "zef/vim-cycle",
-    -- "monaqa/dial.nvim",
+	{
+		"nvim-pack/nvim-spectre",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+  },
+	"zef/vim-cycle",
+
+	-- "monaqa/dial.nvim",
 	-- "alvan/vim-closetag",
 
 	-- FILE MANAGING
@@ -90,10 +91,10 @@ require("lazy").setup({
 	"williamboman/mason.nvim",
 	"williamboman/mason-lspconfig.nvim",
 	"WhoIsSethDaniel/mason-tool-installer.nvim",
-    -- {
-    --     "ray-x/lsp_signature.nvim",
-    --     config = true,
-    -- },
+	-- {
+	--     "ray-x/lsp_signature.nvim",
+	--     config = true,
+	-- },
 	-- 'junnplus/lsp-setup.nvim',
 	-- "jay-babu/mason-null-ls.nvim",
 	-- "jose-elias-alvarez/null-ls.nvim",
@@ -107,74 +108,79 @@ require("lazy").setup({
 		event = "InsertEnter",
 		dependencies = {
 			"hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-nvim-lua",
 			"hrsh7th/cmp-cmdline",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-omni",
-            {
-                "dcampos/cmp-snippy",
-                dependencies = {
-                    {
-                        "dcampos/nvim-snippy",
-                        dependencies = {
-                            "honza/vim-snippets",
-                        }
-                    },
-                }
-            },
+			{
+				"dcampos/cmp-snippy",
+				dependencies = {
+					{
+						"dcampos/nvim-snippy",
+						dependencies = {
+							"honza/vim-snippets",
+						},
+					},
+				},
+			},
 		},
 	},
 
 	-- THEMES
 
-    "SethBarberee/challenger-deep.nvim",
-    {
-        "folke/tokyonight.nvim",
-        lazy = true,
-    },
-    {
-        "NLKNguyen/papercolor-theme",
-        lazy = true,
-    },
-    {
-        "sjl/badwolf",
-        lazy = true,
-    },
-    {
-        "ray-x/aurora",
-        lazy = true,
-    },
-    {
-        "wuelnerdotexe/vim-enfocado",
-        lazy = true,
-    },
-    {
-        "savq/melange",
-        lazy = true,
-    },
-    {
-        "nyoom-engineering/oxocarbon.nvim",
-        lazy = true,
-    },
-    {
-        "EdenEast/Nightfox.nvim",
-        lazy = true,
-    },
-}, {
-	defaults = {
-		-- lazy = false
+  "ShinyZero0/challenger-deep.nvim",
+	{
+		"folke/tokyonight.nvim",
+		lazy = true,
 	},
-})
+	{
+		"NLKNguyen/papercolor-theme",
+		lazy = true,
+	},
+	{
+		"sjl/badwolf",
+		lazy = true,
+	},
+	{
+		"ray-x/aurora",
+		lazy = true,
+	},
+	{
+		"wuelnerdotexe/vim-enfocado",
+		lazy = true,
+	},
+	{
+		"savq/melange",
+		lazy = true,
+	},
+	{
+		"nyoom-engineering/oxocarbon.nvim",
+		lazy = true,
+	},
+	{
+		"EdenEast/Nightfox.nvim",
+    lazy = true,
+  },
+},
+{
+  ui = {
+    border = "rounded",
+  },
+  defaults = {
+    -- lazy = false
+  }
+}
+)
 
 require("config.telescope")
 require("config.pairs")
 require("config.nvim-cmp")
 require("config.lsp")
--- require("config.lspsaga")
-require("config.registers")
 require("config.treesitter")
 require("config.blankline")
+require("config.lspsaga")
+require("config.hydra")
 
 vim.diagnostic.config({
 	virtual_text = false,
@@ -185,8 +191,10 @@ vim.diagnostic.config({
 	float = true,
 })
 
-require('snippy').setup({})
+require("snippy").setup({})
 
 -- require("color-picker").setup({
 -- 	["icons"] = { "ﱢ", "" },
 -- })
+
+-- vim:ts=2:sw=2
