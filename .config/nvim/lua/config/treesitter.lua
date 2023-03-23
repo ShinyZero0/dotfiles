cd11ffdfa59bd187a85f6b56f91d00bdfd05020e
zero mobile <shinyzero0@gmail.com>
2023-03-23 04:09:12 +0300

    assoc empty

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -1,21 +1,20 @@
-require('nvim-treesitter.configs').setup {
-    ensure_installed = { "c", "lua", "vim", "help" },
-    auto_install = true,
-    ignore_install = { "diff", "latex" },
+require("nvim-treesitter.configs").setup({
+	ensure_installed = { "c", "lua", "vim", "help", "python", "c_sharp" },
+	auto_install = false,
+	ignore_install = { "diff", "latex" },
 
-    highlight = {
-        enable = true,
+	highlight = {
+		enable = true,
 
-        disable = { "diff" },
-        disable = function(lang, buf)
-        local max_filesize = 100 * 1024
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
+		disable = { "diff" },
+		disable = function(lang, buf)
+			local max_filesize = 100 * 1024
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			if ok and stats and stats.size > max_filesize then
+				return true
+			end
+		end,
 
-            return true
-            end
-            end,
-
-            additional_vim_regex_highlighting = false,
-            },
-}
+		additional_vim_regex_highlighting = false,
+	},
+})
