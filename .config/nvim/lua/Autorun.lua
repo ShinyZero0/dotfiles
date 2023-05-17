a4c005bbbed9cd432c255bda1f3076cdd80fd0bd
zero <shinyzero0@gmail.com>
2023-05-17 21:48:25 +0300

    features: nvim expandtab on C# ft alias A to vim.api (again)

--- .config/nvim/lua/Autorun.lua
+++ .config/nvim/lua/Autorun.lua
@@ -1,7 +1,17 @@
-vim.api.nvim_create_autocmd("BufEnter", {
+local A = vim.api
+
+A.nvim_create_autocmd("BufEnter", {
 
 	desc = "Rid auto comment for new string",
 	callback = function()
 		vim.opt.formatoptions:remove({ "c", "r", "o" })
+	end,
+})
+
+A.nvim_create_autocmd("BufEnter", {
+
+	pattern = "*.cs",
+	callback = function()
+		vim.o.expandtab = true
 	end,
 })
