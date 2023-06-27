b4e531631372c4c8c351f1d39cef2b6f4ca00009
zero <shinyzero0@gmail.com>
2023-06-27 19:50:53 +0300

    nix hm: add nu scripts to home func params

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, nuScripts, nuTreeSitter, ... }: {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "zero";
