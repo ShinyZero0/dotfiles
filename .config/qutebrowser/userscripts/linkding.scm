ada441f9e18e32039ed2fbb9b5314d512240f347
zero@void <shinyzero0@gmail.com>
2023-08-12 02:53:11 +0300

    qute.linkding: refactor

--- .config/qutebrowser/userscripts/linkding.scm
+++ .config/qutebrowser/userscripts/linkding.scm
@@ -2,115 +2,103 @@
 !#
 
 (use-modules (web client)
-             (zerolib)
-             (scheme base)
-             (srfi srfi-11)
              (json)
-             )
+             (zerolib)
+             (zerolib shell)
+             (scheme base)
+             (srfi srfi-26)
+             (ice-9 receive)
+             (ice-9 threads))
+
 (define (main args)
-  (let ((arg1 (cadr args)))
-  (cond
-    ((equal? arg1 "get") => (lambda (a) (main-get)))
-    ((equal? arg1 "post") => (lambda (a) (main-post)))
-    ((equal? arg1 "delete") => (lambda (_) (main-delete)))
-    )
-  ))
+  (let1 (arg1 (cadr args))
+        (cond
+          ((equal? arg1 "get") => (lambda _ (main-get)))
+          ((equal? arg1 "post") => (lambda _ (main-post)))
+          ((equal? arg1 "delete") => (lambda _ (main-delete))))))
+
 (define (main-post)
-  (define tags
-    (get-tags))
-  (define selected-tags
-    (string-split
-      (call-command-with-input "rofi"
-                               '("-dmenu" "-i" "-multi-select")
-                               (string-join tags "\n"))
-    #\newline))
-  (post-linkding "bookmarks"
-    (scm->json-string
-      `(("url" . ,(getenv "QUTE_URL"))
-        ("tag_names" . ,(list->vector selected-tags)))))
-    )
+  (let*
+    ((tags (get-tags))
+     (selected-tags
+       (split-lines
+         (call-command-with-input
+           "rofi"
+           '("-dmenu" "-i" "-multi-select")
+           tags))))
+    (post-linkding "bookmarks"
+                   (scm->json-string
+                     `(("url" . ,(getenv "QUTE_URL"))
+                       ("tag_names" . ,(list->vector selected-tags)))))))
+
 (define (main-get)
-  (define selected-bmark (select-bmark))
-  (define
-    qute-socket
-    (open-file (getenv "QUTE_FIFO") "w"))
-  (if (not (string-null? selected-bmark))
-    (display
-      (string-append
-        "open -tr "
-        (car
-          (string-split
-            selected-bmark
-            #\tab)))
-      qute-socket))
-  )
+  (let1 (selected-bmark (car (string-split (select-bmark) #\tab)))
+        (if (not (string-null? selected-bmark))
+          (with-output-to-file
+            (getenv "QUTE_FIFO")
+            (lambda _
+              (display
+                (string-append/shared
+                  "open -tr "
+                  selected-bmark)))))))
 (define (main-delete)
   (define selected-bmark (select-bmark))
   (if (not (string-null? selected-bmark))
-  (del-bookmark 
-    (car
-      (reverse
+    (del-bookmark 
+      (last
         (string-split selected-bmark #\tab)))))
-  )
+
 (define (select-bmark)
-  (define bmarks
-    (string-join
-      (map
+  (let1
+    (bmarks
+      (par-map
         (lambda (bmark)
           (string-join
             (list
-             (assoc-ref bmark "url")
-             (assoc-ref bmark "title")
-             (string-join
-               (vector->list
-                 (assoc-ref bmark "tag_names"))
-               ", ")
-             (number->string (assoc-ref bmark "id"))
-             ) "\t"
-            ))
-        (get-bmarks))
-      "\n")
-    )
+              (assoc-ref bmark "url")
+              (assoc-ref bmark "title")
+              (string-join
+                (vector->list
+                  (assoc-ref bmark "tag_names"))
+                ", ")
+              (number->string (assoc-ref bmark "id")))
+            "\t"))
+        (get-bmarks)))
     (call-command-with-input
       "rofi"
-      '("-dmenu" "-i" "-display-columns=url,title,tags")
-      bmarks)
-  )
+      '("-dmenu" "-i" "-display-columns=url,title,tags,id")
+      bmarks)))
+
 (define (get-bmarks)
-  (vector->list (get-linkding "bookmarks")))
+  (get-linkding "bookmarks"))
 (define (get-tags)
-  (map
-    (lambda (tag)
-      (assoc-ref tag "name"))
-    (vector->list
-      (get-linkding "tags"))))
+  (par-map
+    (cute assoc-ref <> "name")
+    (get-linkding "tags")))
+(define (del-bookmark bm)
+  (req-linkding 'DELETE (string-append/shared "bookmarks/" bm)))
 (define (get-linkding endpoint)
+  (vector->list
     (assoc-ref
       (json-string->scm
         (utf8->string
-          (req-linkding 'GET endpoint #f)))
-        "results"))
+          (req-linkding 'GET endpoint)))
+      "results")))
 (define (post-linkding endpoint body)
   (req-linkding 'POST endpoint (string->utf8 body)))
-(define (del-bookmark bm)
-  (req-linkding 'DELETE (string-append "bookmarks/" bm) #f)
-  )
-(define (req-linkding method endpoint body)
-  (let-values
-    (((response resp-body)
-      (http-request
-        (string-append
-          "http://localhost:9090/api/"
-          endpoint
-          "/")
-        #:body body
-        #:method method
-        #:headers
-        `(
-          (Authorization
-            . ,(string-append
-                 "Token "
-                 (getenv "LINKDING_TOKEN")))
-          (Content-Type . "application/json")
-          ))))
+(define*
+  (req-linkding method endpoint #:optional (body #f))
+  (receive
+    (_ resp-body)
+    (http-request
+      (string-append/shared
+        "http://localhost:9090/api/" endpoint "/")
+      #:body body
+      #:method method
+      #:headers
+      `((Authorization
+          . ,(string-append/shared
+               "Token "
+               (getenv "LINKDING_TOKEN")))
+        (Content-Type . "application/json")))
     resp-body))
