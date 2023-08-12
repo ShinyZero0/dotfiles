ec678cb62810a9805967940ae84ce9cebc48135a
zero@void <shinyzero0@gmail.com>
2023-08-12 06:07:33 +0300

    clean up

--- .scripts/DiscordCache.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-discache=~/.cache/discord_cache/
-mkdir -p $discache
-cd $discache
-curr_image=$(echo "$1" | sed "p;s/\//-/g" | sed "N;s/\n/ -O /")
-echo "$curr_image" | xargs wget -nc -q 
-echo "$curr_image" | sed "s/.*-O //" | xargs imv -
--- .scripts/RestartWT.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-pkill -9 aces
-steam steam://rungameid/236390
