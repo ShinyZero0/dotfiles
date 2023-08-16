88ddbd8329cb38e6dbd29d2aa6f9de79b800e297
zero@void <shinyzero0@gmail.com>
2023-08-16 03:41:33 +0300

    guix: add channels

---
+++ .config/guix/channels.scm
@@ -0,0 +1,13 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "f2943958a224beda05a3d040876df2a7285cd7a2")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+
+;; warning: GUIX_PACKAGE_PATH="/home/zero/.config/guix-packages"
