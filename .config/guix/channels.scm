1a606c9b2ab05f3b8a5cb76370a84cdfe6ab64fe
zero@void <shinyzero0@gmail.com>
2023-08-26 15:34:36 +0300

    fixup! guix: add bfs & sxhkd, rm guile-lsp

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -3,11 +3,10 @@
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "f2943958a224beda05a3d040876df2a7285cd7a2")
+          "30487d72cb6d0b602efcd8f0b9af6d042595eb59")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
             (openpgp-fingerprint
               "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
-
 ;; warning: GUIX_PACKAGE_PATH="/home/zero/.config/guix-packages"
